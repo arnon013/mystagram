28ccaaab9de8018aa326c45291dbafddca01be45
Stampp2703 <38807420+Stampp2703@users.noreply.github.com>
2020-11-11 01:08:53 +0700

    addall

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+//3. Configuration here
+const dotenv = require('dotenv').config();
+const Unsplash = require('unsplash-js').default;
+const {toJson} = require('unsplash-js');
+const unsplash = new Unsplash({accessKey:process.env.myAccessKey});
+
+
+//4. add API here
+
+module.exports = router;
